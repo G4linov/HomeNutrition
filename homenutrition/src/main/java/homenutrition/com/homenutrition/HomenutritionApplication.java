46402ae2bdc7df9fb9fde21b57b8d81c8c13311c
Vyacheslavvvv <galin0ff@yandex.ru>
2024-07-06 00:49:01 +0300

    upd project

---
+++ homenutrition/src/main/java/homenutrition/com/homenutrition/HomenutritionApplication.java
@@ -0,0 +1,13 @@
+package homenutrition.com.homenutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomenutritionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomenutritionApplication.class, args);
+	}
+
+}
---
+++ homenutrition/src/test/java/homenutrition/com/homenutrition/HomenutritionApplicationTests.java
@@ -0,0 +1,13 @@
+package homenutrition.com.homenutrition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomenutritionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
