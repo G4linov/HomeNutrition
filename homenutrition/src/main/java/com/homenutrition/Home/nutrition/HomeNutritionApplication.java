bb2afa7846b90520841f4dd76112efa74b04f9c4
Vyacheslavvvv <galin0ff@yandex.ru>
2024-07-06 01:34:57 +0300

    upd proj

---
+++ homenutrition/src/main/java/com/homenutrition/Home/nutrition/HomeNutritionApplication.java
@@ -0,0 +1,13 @@
+package com.homenutrition.Home.nutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeNutritionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeNutritionApplication.class, args);
+	}
+
+}
